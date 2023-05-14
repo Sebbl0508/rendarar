963ed6c899586cf7b499efb1726964f850d9f9b1
Sebastian Rueth <sebastian.rueth@protonmail.com>
2023-05-14 20:15:30 +0200

    include pipeline and shader in public API

--- renderer/src/wgpu/mod.rs
+++ renderer/src/wgpu/mod.rs
@@ -2,6 +2,9 @@ mod context;
 mod pipeline;
 mod shader;
 mod texture;
+mod buffer;
 
 pub use context::WgpuContext;
 pub use texture::Texture;
+pub use pipeline::{RenderPipeline, ShaderSource};
+pub use shader::Shader;
