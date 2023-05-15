54315ca44d12e92bfb7349f602ae2904a5a7971a
Sebastian Rueth <sebastian.rueth@protonmail.com>
2023-05-15 15:41:15 +0200

    add simple buffer wrappers

--- renderer/src/wgpu/mod.rs
+++ renderer/src/wgpu/mod.rs
@@ -1,10 +1,10 @@
+mod buffer;
 mod context;
 mod pipeline;
 mod shader;
 mod texture;
-mod buffer;
 
 pub use context::WgpuContext;
-pub use texture::Texture;
 pub use pipeline::{RenderPipeline, ShaderSource};
 pub use shader::Shader;
+pub use texture::Texture;
